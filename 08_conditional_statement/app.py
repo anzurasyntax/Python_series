94b479da4a0a9f34b76ceac58e79dfeecdcb031c
anzurasyntax <anzurasyntax@gmail.com>
2025-12-19 17:18:51 +0500

    add operators and conditional statements

---
+++ 08_conditional_statement/app.py
@@ -0,0 +1,23 @@
+# Conditional Statements
+
+
+# if statement
+x = 10
+if x > 5:
+    print("x is greater than 5")
+
+# if-else statement
+y = 3
+if y < 5:
+    print("y is less than 5")
+else:
+    print("y is not less than 5")
+
+# else statement
+z = 2
+if z == 5:
+    print("z is equal to 5")
+elif z > 5:
+    print("z is greater than 5")
+else:
+    print("z is less than 5")
